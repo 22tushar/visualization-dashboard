b9f5382b9f357bfb578b32aa882f3e5944d43386
unknown <tushargehlot123@gmail.com>
2023-09-14 16:59:45 +0530

    add api

--- Dashboard-master/src/components/BarChart.tsx
+++ Dashboard-master/src/components/BarChart.tsx
@@ -1,28 +1,51 @@
-import React from 'react';
+
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import React, { useState, useEffect } from 'react';
 
 const BarCharts = () => {
  
-     const data = [
-         {
-            topic:"topic:oil",
-            country:3,
-            fee:"xx"
-         },
-         {
-          topic:"topic:gas",
-            country:10,
-            fee:"yy"
-         },
-         {
-          topic:"topic:gdp",
-          country:10,
-          fee:"zz"
-       }
-     ]
+  const [MongoBarData, setData] = useState([
+    {
+      topic:"topic:oil",
+      country:3,
+      fee:"xx"
+   },
+   {
+     topic:"topic:gas",
+      country:10,
+      fee:"yy"
+   },
+   {
+    topic:"topic:gdp",
+    country:10,
+    fee:"zz"
+ }
+  ])
+
+    // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('https://api.example.com/data'); // Replace with your API endpoint
+  //       const result = await response.json();
+  //       setData(result);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);  
+
+     const BarData= MongoBarData.map(item => {
+      return {
+        topic: item.topic,
+        // color: `hsl(${Math.random() * 360}, 70%, 50%)`, // Assign random color or use a logic to generate unique colors
+        country:item.country
+      }
+    });
   return (
      <ResponsiveContainer width="50%" aspect={3}>
-         <BarChart data={data} width={400} height={400}>
+         <BarChart data={BarData} width={400} height={400}>
           <XAxis dataKey='topic'/>
           <YAxis/>
           <Tooltip/>
